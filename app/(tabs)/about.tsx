5387d10feeebcb58273e13431b911068e6aaa492
david <davidobinna579@gmail.com>
2024-06-30 09:48:39 +0100

    first commit

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,19 @@
+
+import Hero from '@/components/Hero';
+import { router } from 'expo-router';
+
+export default function AboutScreen() {
+
+  return (
+          <Hero
+           title="welcome to my app"
+           buttonText="Pls Sign in"
+           onButtonPress={() => {} }
+          >
+          </Hero>
+  );
+}
+
+
+//installing universal app
+//https://dev.to/hasan_py/setup-)your-universal-app-with-react-native-expo-router-tamagui-and-storybook-28m6
---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+
+type HeroProps = {
+    title: string
+    buttonText: string
+    onButtonPress: () => void
+}
+
+
+
+const Hero: React.FC<HeroProps>  = (props) => {
+    const {title, buttonText, onButtonPress} = props;
+
+    return <View> 
+             <Text style={styles.title}>{title}</Text>
+             <Button title={buttonText} onPress={onButtonPress} ></Button>
+          </View>
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 32, 
+        marginBottom: 10,
+        textAlign: 'center'
+    }
+})
+
+export default Hero;
